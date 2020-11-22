d7714d0f09d7ae400023e93693611756fc20c399
accubility <joshua.obando98@gmail.com>
2020-11-21 20:54:41 -0500

    added code from activities to see if it would work but did not, will remove if nothing else works

--- logic.js
+++ logic.js
@@ -1,67 +1,70 @@
-const fs = require("fs")
-
 const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-inquirer
-    .prompt([{
-            type: "input",
-            message: "What is your GitHub username?",
-            name: "username",
-            default: "joshobando",
-        },
-        {
-            type: "input",
-            message: "What is your email address?",
-            name: "email",
-            default: "joshua.obando98@gmail.com",
-        },
-        {
-            type: "input",
-            message: "What is your project's name?",
-            name: "project",
-            default: "READMEgen homework",
-        },
-        {
-            type: "input",
-            message: "Please write a short description of your project",
-            name: "description",
-            default: "Very descrptive description"
-        },
-        {
-            type: "list",
-            message: "What kind of license should your project have?",
-            name: "license",
-            choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
-            default: "None",
-        },
-        {
-            type: "input",
-            message: "What command should be run to install dependencies?",
-            name: "install",
-            default: "npm i inquirer",
-        },
-        {
-            type: "input",
-            message: "What command should be run to run tests?",
-            name: "run",
-            default: "node logic.js",
-        },
-        {
-            type: "input",
-            message: "What does the user need to know about using the repo?",
-            name: "using",
-            default: "I did my homework"
-        },
-        {
-            type: "input",
-            message: "What does the user need to know about contributing to the repo?",
-            name: "contributing",
-            default: "Not sure yet",
-        }
-    ])
+const writeFileAsync = util.promisify(fs.writeFile);
 
-function writetofile(filename, data) {
-    fs.writeFile(filename, data, error => {
+const promptUser = () => 
+    inquirer.prompt([
+    {    
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username",
+        default: "joshobando",
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+        default: "joshua.obando98@gmail.com",
+    },
+    {
+        type: "input",
+        message: "What is your project's name?",
+        name: "project",
+        default: "READMEgen homework",
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project",
+        name: "description",
+        default: "Very descrptive description"
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        name: "license",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+        default: "None",
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "install",
+        default: "npm i inquirer",
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "run",
+        default: "node logic.js",
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "using",
+        default: "I did my homework"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo?",
+        name: "contributing",
+        default: "Not sure yet",
+    },
+    ]);
+
+function writetofile(READMEgen, data) {
+    fs.writeFile(READMEgen, data, error => {
         if (error) {
             return console.log(error);
         }
